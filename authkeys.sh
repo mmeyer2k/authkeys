d14feb467b5e69db4024aa0981e8ba0e65d72cac
mike <m.meyer2k@gmail.com>
2021-11-04 02:27:30 -0500

    add history folder feature

--- authkeys.sh
+++ authkeys.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 KEYSFILE="$HOME/.ssh/authorized_keys"
-KEYSTEMP="$HOME/.ssh/authorized_keys.overwrite"
+KEYSTEMP="$KEYSFILE.overwrite"
+KEYSHISTDIR="$KEYSFILE.history"
 while :
 do
+    KEYSHIST="$KEYSHISTDIR/$(date +%s)"
     clear
     if [ ! -w "$KEYSFILE" ]; then
         echo "Authorized keys [$KEYSFILE] file does not exist. It will be created upon save."
@@ -28,17 +30,27 @@ do
     echo "1: Add new key"
     echo "2: Delete an existing key"
     echo "3: Exit"
+    if [ -d $KEYSHISTDIR ]; then
+        echo "4: Remove temp dir [$KEYSHISTDIR]"
+    fi
     echo
     read -p "Select option: " MENU
     if [ "$MENU" == "1" ]; then
         read -s -p "Enter new public key: " NEWKEY
+        if [ -z "$NEWKEY" ]; then
+            echo
+            echo "Invalid entry!";
+            sleep 2
+            continue
+        fi
         echo
         echo
         echo $NEWKEY
         echo
         read -p "Are you sure you would like to add this key? [y/N]" SURE
         if [ "$SURE" == "y" ]; then
-            touch "$KEYSFILE"
+            mkdir -p "$KEYSHISTDIR"
+            cp "$KEYSFILE" "$KEYSHIST"
             echo "$NEWKEY" >> "$KEYSFILE"
             echo "Key has been added!";
             sleep 2
@@ -49,24 +61,30 @@ do
         if ((DELKEY < 0 || DELKEY >= ${#KEYSDATA[@]})); then
             echo "Invalid key number."
             sleep 2
-        else
-            echo
-            echo ${KEYSDATA[$DELKEY]}
-            echo
-            read -p "Delete this key? [y/N] " CONFIRMDELETE
-            if [ "$CONFIRMDELETE" == "y" ]; then
-                unset KEYSDATA[$DELKEY]
-                for LINE in "${KEYSDATA[@]}"; do
-                    echo "$LINE" >> "$KEYSTEMP"
-                done
-                mv -f "$KEYSTEMP" "$KEYSFILE"
-                echo "Key has been deleted!";
-                sleep 2
-            fi
+            continue
+        fi
+        echo
+        echo ${KEYSDATA[$DELKEY]}
+        echo
+        read -p "Delete this key? [y/N] " CONFIRMDELETE
+        if [ "$CONFIRMDELETE" == "y" ]; then
+            mkdir -p "$KEYSHISTDIR"
+            cp "$KEYSFILE" "$KEYSHIST"
+            unset KEYSDATA[$DELKEY]
+            for LINE in "${KEYSDATA[@]}"; do
+                echo "$LINE" >> "$KEYSTEMP"
+            done
+            mv -f "$KEYSTEMP" "$KEYSFILE"
+            echo "Key has been deleted!";
+            sleep 2
         fi
     elif [ "$MENU" == "3" ]; then
         echo "Exiting..."
         exit
+    elif [ "$MENU" == "4" ]; then
+        rm -rf "$KEYSHISTDIR"
+        echo "Key history cleared!"
+        sleep 2
     else
         echo "Invalid option"
         sleep 2
